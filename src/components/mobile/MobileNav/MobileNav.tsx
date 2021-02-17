057cd8ff78750c4b3653835b32cea7b6e0d8d177
Zakhar Petrushin <z.petrushin@pcnp-studio.ru>
2021-02-17 23:19:16 +0300

    fixed mobile menu

--- src/components/mobile/MobileNav/MobileNav.tsx
+++ src/components/mobile/MobileNav/MobileNav.tsx
@@ -2,14 +2,20 @@ import React, { useState } from 'react'
 import Link from 'next/link'
 import cn from 'classnames'
 
+import { Button } from '../../Button/Button'
+
 import styles from './MobileNav.module.sass'
 
 
+const icon = require('src/common/images/icons/mail.png')
 const openMenuIcon = require('src/common/images/menu/openMenuIcon.png')
 const closeMenuIcon = require('src/common/images/menu/closeMenuIcon.png')
 
 
 export const MobileNav: React.FC = () => {
+  const handlerButton = () => {
+
+  }
   const [isOpen, setIsOpen] = useState(false)
   return (
     <>
@@ -33,6 +39,12 @@ export const MobileNav: React.FC = () => {
         <Link href='/'><a>Технологии</a></Link>
         <Link href='/'><a>Команда</a></Link>
         <Link href='/'><a>Контакты</a></Link>
+        <Button
+          onClick={handlerButton}
+          btnType='outline'
+        >
+          <img src={icon} />
+        </Button>
       </nav>
     </>
   )
