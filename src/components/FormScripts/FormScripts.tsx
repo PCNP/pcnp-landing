b13a89c499177a0f8fdd7e75aea9ff651ee537b2
Zakhar Petrushin <z.petrushin@pcnp-studio.ru>
2021-03-05 23:23:09 +0300

    fix: qForm24_2

--- src/components/FormScripts/FormScripts.tsx
+++ src/components/FormScripts/FormScripts.tsx
@@ -10,9 +10,28 @@ export const FormScripts: React.FC = () => {
         dangerouslySetInnerHTML={
           {
             __html: `
-              function init() {var scr = document.createElement('script'); scr.type = 'text/javascript'; scr.defer = 'defer'; scr.src = '//cdn.qform24.com/forms.js?v=' + parseInt(new Date().getTime()/1000); var scrInsert = document.getElementsByTagName('script')[0]; scrInsert.parentNode.insertBefore(scr, scrInsert); }
-            var d = document; var w = window;
-            if (d.readyState == 'complete') {init(); } else {if (w.attachEvent) {w.attachEvent('onload', init); } else {w.addEventListener('load', init, false); } } })();
+              function init() {
+                var scr = document.createElement('script');
+                scr.type = 'text/javascript';
+                scr.defer = 'defer';
+                scr.src = '//cdn.qform24.com/forms.js?v=' + parseInt(new Date().getTime()/1000);
+                var scrInsert = document.getElementsByTagName('script')[0];
+                scrInsert.parentNode.insertBefore(scr, scrInsert);
+              }
+              var d = document;
+              var w = window;
+              if (d.readyState == 'complete') {
+                init();
+              }
+              else {
+                if (w.attachEvent) {
+                  w.attachEvent('onload', init);
+                }
+                else {
+                  w.addEventListener('load', init, false);
+                 }
+              }
+             )();
             `,
           }
         }
