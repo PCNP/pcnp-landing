e5e3519b4eaa1da519b4e532c68e9c7ab932ee2b
Zakhar Petrushin <z.petrushin@pcnp-studio.ru>
2021-03-13 01:14:17 +0300

    fix: lang

--- out/_next/static/VcqvNF5z6TIP_jtr-7EvS/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fen\u002Fportfolio","\u002Fportfolio","\u002Fen","\u002F","\u002Fen\u002Fportfolio\u002F[slug]","\u002Fportfolio\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
